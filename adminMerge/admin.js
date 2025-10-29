014fdc03c9318d43e0d3fec0639f055f613b9033
Dev-Huy <acc.clone.0309@gmail.com>
2025-10-29 20:23:36 +0700

    Delete adminMerge directory
    replace legacy

--- adminMerge/admin.js
+++
@@ -1,4 +0,0 @@
-const sideBarBtnAll = document.querySelectorAll("#side_bar_btn a");
-sideBarBtnAll.forEach(sideBarBtnA => sideBarBtnA.addEventListener("click", () => {
-    sideBarBtnA.classList.toggle("active")
-}))
