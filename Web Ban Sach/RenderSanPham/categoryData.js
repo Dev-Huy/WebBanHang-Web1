62d9fbc05deebe42b76119ced88cd12d735f528b
TTK190706 <ttk2k61907@gmail.com>
2025-11-04 21:03:15 +0700

    Add files via upload
    Push phần render sản phẩm

---
+++ Web Ban Sach/RenderSanPham/categoryData.js
@@ -0,0 +1,7 @@
+export const categoryList = [
+  { id: "tlh", name: "Tâm Lý Học" },
+  { id: "vh", name: "Văn Học" },
+  { id: "sgk", name: "Sách Giáo Khoa" },
+  { id: "khcn", name: "Khoa Học - Công Nghệ" },
+  { id: "kt", name: "Kinh Tế" }
+];
